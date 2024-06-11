7b2c17fb925961e8cd240bc24390be3620f99638
Elvin Topalovic <166206476+elvi93@users.noreply.github.com>
2024-06-11 20:31:51 +0200

    done

---
+++ src/components/Tweet.vue
@@ -0,0 +1,30 @@
+<script setup>
+import Actions from "./TweetComps/Actions.vue";
+import Message from "./TweetComps/BodyMessage.vue";
+import User from "./TweetComps/User.vue";
+import ProfileImage from "./TweetComps/ProfileImage.vue";
+
+defineProps({
+  tweet: Object,
+});
+</script>
+
+<template>
+  <div class="tweet">
+    <ProfileImage v-bind:image="tweet.user.image" />
+
+    <div class="body">
+      <User v-bind:user="tweet.user" v-bind:timeStamp="tweet.timestamp" />
+      <Message v-bind:message="tweet.message" />
+      <Actions />
+    </div>
+
+    <i class="fas fa-ellipsis-h"></i>
+  </div>
+</template>
+
+<style scoped>
+a {
+  color: #42b983;
+}
+</style>
---
+++ src/components/TweetComps/User.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="top">
+    <span class="user">
+      <span class="name">{{ user.name }} </span>
+      <span class="handle">@{{ user.handle }}</span>
+    </span>
+
+    <Time v-bind:timeStamp="timeStamp" />
+  </div>
+</template>
+
+<script setup>
+import Time from "./TimeStamp.vue";
+
+defineProps({
+  user: Object,
+  timeStamp: String,
+});
+</script>
+
+
